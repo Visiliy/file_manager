aee65e08e522c843a8d3426f359d829f8690bffd
user <irin-shkuratov8@yandex.ru>
2024-09-09 19:54:01 +0300

    Your comment

---
+++ main.py
@@ -0,0 +1,10 @@
+import eel
+
+def close_callback(route, websockets):
+    print(route)
+    if not websockets:
+        exit()
+
+if __name__ == "__main__":
+    eel.init("vievs")
+    eel.start("index.html", size=(1900, 1000), close_callback=close_callback)
