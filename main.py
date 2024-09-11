d5fe2adfae4a1f30d1eb9244b1807c67b51e2f57
user <irin-shkuratov8@yandex.ru>
2024-09-11 14:47:11 +0300

    /

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import eel
+from tkinter import Tk
+
+root = Tk()
+WIDTH = root.winfo_screenwidth()
+HEIGHT = root.winfo_screenheight()
 
 def close_callback(route, websockets):
-    print(route)
     if not websockets:
         exit()
 
 if __name__ == "__main__":
     eel.init("Frontend")
-    eel.start("index.html", size=(1900, 1000), close_callback=close_callback)
+    eel.start("index.html", size=(WIDTH, HEIGHT), close_callback=close_callback)
